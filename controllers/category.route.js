60d383a2d833871bc31aab2d5d26054665191f9b
Thanh Quang <66661355+anhhoi35bn@users.noreply.github.com>
2021-07-05 23:15:50 +0700

    Starting to create a MVC web app. Create a init route (root directory) and fix some bugs from css/js/img folder

---
+++ controllers/category.route.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const categoryModel = require('../models/category.model');
+
+const router = express.Router();
+
+router.get('/', function(req, res) {
+    const list = categoryModel.all(); //View model
+
+
+
+    res.render('vwCategories/index', { // Controller
+        categories: list,
+        empty: list.length === 0
+    });
+
+
+});
+
+router.get('/add', function(req, res) {
+    res.render('vwCategories/add');
+})
+
+router.post('/add', function(req, res) {
+    const new_category = {
+        CatID: -1,
+        CatName: req.body.txtCatName
+    };
+
+
+    categoryModel.add(new_category);
+    res.render('vwCategories/add');
+
+})
+
+module.exports = router;
