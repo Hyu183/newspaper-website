3faf6eb1f7aa498b1799c347e8b28fa56f419d84
l4zyc0d3r <huytrquang1103@gmail.com>
2021-08-02 08:30:24 +0700

    edit css

--- controllers/admin.route.js
+++ controllers/admin.route.js
@@ -10,19 +10,31 @@ const router = express.Router();
 // })
 
 router.get('/',function(req,res){
-    res.render('vwAdmin/users', {layout: 'admin.hbs'});
+    res.render('vwAdmin/users', {
+        layout: 'admin.hbs',
+        userActive: true
+    });
 })
 
 router.get('/tags',function(req,res){
-    res.render('vwAdmin/tags', {layout: 'admin.hbs'});
+    res.render('vwAdmin/tags', {
+        layout: 'admin.hbs',
+        tagActive: true
+    });
 })
 
 router.get('/posts',function(req,res){
-    res.render('vwAdmin/posts', {layout: 'admin.hbs'});
+    res.render('vwAdmin/posts', {
+        layout: 'admin.hbs',
+        postActive: true
+    });
 })
 
 router.get('/categories',function(req,res){
-    res.render('vwAdmin/categories', {layout: 'admin.hbs'});
+    res.render('vwAdmin/categories', {
+        layout: 'admin.hbs',
+        categoryActive: true
+    });
 })
 
 module.exports = router;
