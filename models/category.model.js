c166a44471e27a24b57bd93bd89f277e0b356a9d
l4zyc0d3r <huytrquang1103@gmail.com>
2021-08-04 11:02:38 +0700

    add query for: allMainCategories, allSubCategories, findByParentID

--- models/category.model.js
+++ models/category.model.js
@@ -6,7 +6,24 @@ module.exports = {
         return db.select().from('category');
     },
 
+    allMainCategories(){
+        return db.select()
+                .from('category')
+                .whereNull('parent_id');
+    },
+
+    allSubCategories(){
+        return db.select()
+                .from('category')
+                .whereNotNull('parent_id');
+    },
+
     add(category) {
-        list.push(category);
+        return db('category').insert(category);
+    },
+
+    findByParentID(parent_id){
+        return db('category')
+                .where('parent_id',parent_id);
     }
 };
