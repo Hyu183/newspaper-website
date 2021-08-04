c481ffd290784217839d14b40917b980f5a19da5
l4zyc0d3r <huytrquang1103@gmail.com>
2021-08-04 11:04:41 +0700

    add function for add category button and show list category

--- controllers/admin.route.js
+++ controllers/admin.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const categoryModel = require('../models/category.model');
+const categoryModel = require('../models/category.model');
 
 const router = express.Router();
 
@@ -30,10 +30,46 @@ router.get('/posts',function(req,res){
     });
 })
 
-router.get('/categories',function(req,res){
+router.get('/categories',async function(req,res){
+    const categories = await categoryModel.all();
+
     res.render('vwAdmin/categories', {
         layout: 'admin.hbs',
-        categoryActive: true
+        categoryActive: true,
+        categories
+    });
+})
+
+router.get('/categories/add',async function(req,res){
+    const mainCategories = await categoryModel.allMainCategories();
+    res.render('vwAdmin/addCategory', {
+        layout: 'admin.hbs',
+        categoryActive: true,
+        mainCategories
+    });
+})
+
+router.get('/is-category-available',async function(req,res){
+    const title = req.query.title;
+    const parent_id = req.query.parent_id;
+    const list = await categoryModel.findByParentID(parent_id);
+    let titles = []
+    for( c of list){
+        titles.push(c.title.toLowerCase());
+    }
+
+    res.json(!titles.includes(title.toLowerCase()));
+})
+
+router.post('/categories/add',async function(req,res){
+    const new_category = req.body;
+    await categoryModel.add(new_category);
+    
+    const mainCategories = await categoryModel.allMainCategories();
+    res.render('vwAdmin/addCategory', {
+        layout: 'admin.hbs',
+        categoryActive: true,
+        mainCategories
     });
 })
 
