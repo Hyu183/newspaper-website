2131af26d646e1bfccde65657a7af2de300caccb
Nguyen Duong <nguyenduongag@gmail.com>
2021-08-10 22:39:14 +0700

    add login function

--- models/user.model.js
+++ models/user.model.js
@@ -5,14 +5,39 @@ const addUser = (user) => {
     return db('users').insert(user);
 };
 
+const checkAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/user/sign_in');
+}
+
+const checkNotAuthenticated = (req, res, next) => {
+    console.log("here12");
+    if (req.isAuthenticated()){
+        res.redirect('/');
+    }
+    return next();
+}
+
 module.exports = {
     addUser,
-    
     async findByUsername(username){
         const rows = await db('users').where('user_name', username);
          if(rows.length===0){
-             return null;
+            return null;
          }
          return rows[0];
      },
+
+     async getUserbyId(id){
+        const rows = await db('users').where('id', id);
+         if(rows.length===0){
+            return null;
+         }
+         return rows[0];
+     },
+
+     checkAuthenticated,
+     checkNotAuthenticated
 };
---
+++ public/js/config/passport.config.js
@@ -0,0 +1,36 @@
+const LocalStrategy = require('passport-local').Strategy;
+const {findByUsername, getUserbyId} = require('../../../models/user.model');
+const bcrypt = require('bcryptjs');
+
+const initialize = (passport) => {
+    const authenticateUser = async (username, password, done) => {
+        console.log(username);
+        console.log(password);
+
+        const user = await findByUsername(username);
+        if (user == null){
+            return done(null, false, {message: "Wrong username or password"});
+        }
+
+        try {
+            if (await bcrypt.compare(password, user.password)){
+                console.log("login successful");
+                return done(null, user);
+            } else {
+                return done(null, false, {message: "Wrong username or password"});
+            }
+        } catch (error) {
+            return done(error);
+        }
+    } 
+
+    passport.use(new LocalStrategy({ usernameField: 'user_name'}, authenticateUser));
+
+    passport.serializeUser( (user, done) => done(null, user.id) );
+
+    passport.deserializeUser( (id, done) => {
+        return done(null, getUserbyId(id));
+    });
+};
+
+module.exports = initialize;
