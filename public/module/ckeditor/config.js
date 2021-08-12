9e49c437960ce8abed89bd486fc9400d1cb0242e
Nguyen Duong <nguyenduongag@gmail.com>
2021-08-12 23:17:32 +0700

    add uploading video

--- public/module/ckeditor/config.js
+++ public/module/ckeditor/config.js
@@ -36,5 +36,5 @@ CKEDITOR.editorConfig = function( config ) {
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
 
-	config.filebrowserUploadMethod = 'form';
+	config.embed_provider = '//ckeditor.iframe.ly/api/oembed?url={url}&callback={callback}'
 };
