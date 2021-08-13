43a8d0a7fcd46a5417bda0981f516050956f8e2f
l4zyc0d3r <huytrquang1103@gmail.com>
2021-08-13 09:16:53 +0700

    edit res.locals.lcMainCategories = main + sub category

--- middlewares/locals.mdw.js
+++ middlewares/locals.mdw.js
@@ -4,7 +4,11 @@ module.exports = function(app){
     app.use(async function(req,res,next){
         const categories = await categoryModel.all();
         const mainCategories = await categoryModel.allMainCategories();
-
+        for(c of mainCategories){
+            const subCat = await categoryModel.findByParentID(c.id);
+            c.subCat = subCat;
+        }
+        
         res.locals.lcCategories = categories;
         res.locals.lcMainCategories = mainCategories;
         next();
